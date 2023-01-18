32442957cf0d5feb00a12054cfc6495e43d26582
hnnav <57915266+hnnav@users.noreply.github.com>
2023-01-18 14:15:20 +0200

    puppeteer scraping

--- server/index.js
+++ server/index.js
@@ -3,16 +3,35 @@ const app = express()
 const axios = require('axios')
 const cors = require('cors')
 const { parseString } = require('xml2js')
+const socket = require("socket.io");
 
 // middlewares
 app.use(express.json());
 app.use(cors());
 
-// Getting update only when restarting server
-setInterval(() => {
-    axios('https://assignments.reaktor.com/birdnest/drones')
-        .then(res =>  parseString(res.data, function (err, result) {renderThis(result)}))
-}, 1000);
+// axios('https://assignments.reaktor.com/birdnest/drones')
+//     .then(res =>  parseString(res.data, function (err, result) {renderThis(result)}))
+
+// Trying fetch with puppeteer
+const puppeteer = require('puppeteer'); 
+ 
+(async () => { 
+	const browser = await puppeteer.launch(); 
+	const page = await browser.newPage(); 
+	await page.goto('https://assignments.reaktor.com/birdnest/drones')
+
+    const data = await page.evaluate(() => {
+        all_elements = document.querySelectorAll('capture drone *');
+        text_array = Array.from(all_elements);
+        return text_array.map(text => text.textContent);
+    });
+
+    console.log(data)
+
+    renderThis(data)
+
+	await browser.close(); 
+})();
 
 const renderThis = (result) => {
     app.use("/", async (req, res) => {
@@ -25,3 +44,10 @@ const PORT = 8080
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 });
+
+// // Socket setup
+// const io = socket(server);
+// 
+// io.on("connection", function (socket) {
+//     console.log("Made socket connection");
+// });
